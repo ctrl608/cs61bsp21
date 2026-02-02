cab5fa895d810b2e6047c05f829beca7e8e4fdfb
12paterium <flying_boat@126.com>
2026-02-02 19:16:01 +0800

    proj1 第7次

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -3,15 +3,22 @@ package deque;
 import java.util.Iterator;
 
 public interface Deque<T> {
-	 void addFirst(T item);
-	 void addLast(T item);
+	void addFirst(T item);
+
+	void addLast(T item);
+
 	default boolean isEmpty() {
-		return size()==0;
+		return size() == 0;
 	}
-	 int size();
-	 void printDeque();
+
+	int size();
+
+	void printDeque();
+
 	T removeFirst();
+
 	T removeLast();
+
 	T get(int index);
 
 	Iterator<?> iterator();
